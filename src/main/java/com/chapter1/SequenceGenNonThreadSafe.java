f9066bbea193c9b2f3a730871537c84d6199ee9f
Rahul Deore <rahul.deore@outlook.com>
2022-01-07 16:42:29 -0500

    First Commit

---
+++ src/main/java/com/chapter1/SequenceGenNonThreadSafe.java
@@ -0,0 +1,55 @@
+package com.chapter1;
+
+import com.rahul.annotations.NotThreadSafe;
+
+/**
+ * Created by Rahul Deore on 1/1/22
+ *
+ * This non threadsafe method getNext() can be made thread safe by adding synchronized keyword
+ */
+@NotThreadSafe
+public class SequenceGenNonThreadSafe {
+
+    static int val=0;
+    public static int getNext() {
+        return val++;
+    }
+    public static int getVal() {
+        return val;
+    }
+    public static void main(String... args) {
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0; i<10000; i++) {
+                    System.out.println(getNext());
+                }
+            }
+        });
+        t1.start();
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0; i<10000; i++) {
+                    System.out.println(getNext());
+                }
+            }
+        });
+        t2.start();
+
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0; i<10000; i++) {
+                    System.out.println(getNext());
+                }
+            }
+        });
+        t3.start();
+
+        System.out.println("The val: " + getVal());
+    }
+
+}
---
+++ src/main/java/com/rahul/annotations/NotThreadSafe.java
@@ -0,0 +1,4 @@
+package com.rahul.annotations;
+
+public @interface NotThreadSafe {
+}
